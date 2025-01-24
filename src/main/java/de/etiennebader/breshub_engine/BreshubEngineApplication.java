be55e2b65e39afc2b54b27a267c6ab554a6009d6
Etienne Bader <etienne.bader@atruvia.de>
2025-01-24 23:08:02 +0100

    feat: initial commit

---
+++ src/main/java/de/etiennebader/breshub_engine/BreshubEngineApplication.java
@@ -0,0 +1,13 @@
+package de.etiennebader.breshub_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreshubEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreshubEngineApplication.class, args);
+	}
+
+}
