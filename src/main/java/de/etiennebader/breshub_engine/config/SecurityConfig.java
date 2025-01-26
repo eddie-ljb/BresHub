14bf8aa7694ac28350a3dc809d8e4092301f36ef
Etienne Bader <baderetienne@gmail.com>
2025-01-26 17:21:44 +0100

    feat: added test Controller

---
+++ src/main/java/de/etiennebader/breshub_engine/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package de.etiennebader.breshub_engine.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+public class SecurityConfig {
+}
--- src/main/java/de/etiennebader/breshub_engine/controller/TestController.java
+++ src/main/java/de/etiennebader/breshub_engine/controller/TestController.java
@@ -1,13 +1,14 @@
 package de.etiennebader.breshub_engine.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
-    @RequestMapping("/helloworld")
+    @GetMapping("/helloworld")
     public String helloWorld() {
         return "Hello World";
     }
