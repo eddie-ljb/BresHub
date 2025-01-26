15fe0d05f8f400925d33dc05d47178a5351ef73a
Etienne Bader <baderetienne@gmail.com>
2025-01-26 18:07:22 +0100

    feat: added Swagger and DB configs

--- src/main/java/de/etiennebader/breshub_engine/BreshubEngineApplication.java
+++ src/main/java/de/etiennebader/breshub_engine/BreshubEngineApplication.java
@@ -2,9 +2,12 @@ package de.etiennebader.breshub_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EntityScan(basePackages = "de.etiennebader.breshub_engine.dao")
+
 public class BreshubEngineApplication {
 
 	public static void main(String[] args) {
--- src/main/java/de/etiennebader/breshub_engine/controller/TestController.java
+++ src/main/java/de/etiennebader/breshub_engine/controller/TestController.java
@@ -3,12 +3,14 @@ package de.etiennebader.breshub_engine.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/test")
+@RestController
+@RequestMapping(path = "/test")
 public class TestController {
 
-    @GetMapping("/helloworld")
+    @GetMapping(path = "/helloworld")
     public String helloWorld() {
         return "Hello World";
     }
