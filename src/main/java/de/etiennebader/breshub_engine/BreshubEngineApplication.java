4962a78b8d42eab381d669be984c522b6eef9681
Etienne Bader <baderetienne@gmail.com>
2025-02-13 23:25:13 +0100

    refactor: port change

--- src/main/java/de/etiennebader/breshub_engine/BreshubEngineApplication.java
+++ src/main/java/de/etiennebader/breshub_engine/BreshubEngineApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.util.Collections;
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EntityScan(basePackages = "de.etiennebader.breshub_engine.dao")
 
 public class BreshubEngineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BreshubEngineApplication.class, args);
+		SpringApplication app = new SpringApplication(BreshubEngineApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8084"));
+		app.run(args);
 	}
 
 }
